4149c253c9488f0c73dabf6bd1c5804c7341e380
Indhirajith S <727824TUAD052@skct.edu.in>
2026-07-29 14:18:45 +0530

    User commit

---
+++ backend/src/main/java/com/example/demo/DraftDashApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DraftDashApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DraftDashApplication.class, args);
+    }
+}
