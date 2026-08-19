5b40562845d61e1dd264fdc74876d77e6aa22e23
Indhirajith S <727824TUAD052@skct.edu.in>
2026-08-19 10:06:45 +0530

    User commit

--- frontend/src/App.test.js
+++
@@ -1,7 +0,0 @@
-// src/testcase/App.test.js
-import { render } from '@testing-library/react';
-import App from '../App'; // Adjust this path if your App.js is somewhere else
-
-test('renders the App component', () => {
-  render(<App />);
-});
